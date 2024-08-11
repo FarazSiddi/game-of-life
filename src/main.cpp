5519f893dc9206b489e1fed41f5907694cd3d962
FarazSiddi <f1.siddiqui@yandex.com>
2024-08-10 23:39:58 -0400

    Fixed Compiling Issues

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <raylib.h>
+
+int main()
+{
+    Color darkGreen = Color{20, 160, 133, 255};
+
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    CloseWindow();
+    return 0;
+}
